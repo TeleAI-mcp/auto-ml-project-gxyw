ef20218cb8017919a3605981ef775aeb419b2075
TeleAI-mcp <teleaitest@163.com>
2026-02-21 02:03:14 +0800

    Add placeholder for global_settings.py from external repo

--- django/conf/global_settings.py
+++ django/conf/global_settings.py
@@ -1,3 +1,3 @@
-# This file was intended to be copied from django/django repository
-# but the content could not be retrieved. Please manually copy the content
-# from https://github.com/django/django/blob/main/django/conf/global_settings.py
+# Placeholder for global_settings.py copied from django/django
+# Original file at: https://github.com/django/django/blob/main/django/conf/global_settings.py
+# Note: The actual content could not be copied via tools due to limitations in retrieving external file content.
