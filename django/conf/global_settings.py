419e9ab1967bc33b9d5cc60d7eb9100e4fd63fd3
TeleAI-mcp <teleaitest@163.com>
2026-02-21 01:43:37 +0800

    Add placeholder for django global_settings.py

---
+++ django/conf/global_settings.py
@@ -0,0 +1,3 @@
+# This file was intended to be copied from django/django repository
+# but the content could not be retrieved. Please manually copy the content
+# from https://github.com/django/django/blob/main/django/conf/global_settings.py
